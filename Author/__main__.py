9cea17dc4285158c8fe106465ff46d3669366af0
SoumitraAgarwal <agarwalsoumitra1504@gmail.com>
2017-08-16 09:33:52 +0530

    Final changes to author

--- Author/__main__.py
+++ Author/__main__.py
@@ -1,5 +1,22 @@
+import pandas as pd
 import process
+import random
+import time
+import os
 
+# Get train text
+millis 			= int(round(time.time() * 1000))
+files 			= os.listdir("../Song Lyrics/Data/Lyrics")
+files			= random.sample(files, 5)
+LyricsData		= []
+
+for file in files:
+	
+	df		= pd.read_csv("../Song Lyrics/Data/Lyrics/" + file)
+	song 	= ['.'.join(x.splitlines()) for x in df["Lyrics"]]
+	LyricsData.append('\n'.join(song))
+
+train 				= '\n'.join(LyricsData)
 train 			= process.clean_file(train)
 
 mapFirsts, mapCentres, mapLasts, Firsts, Centres, Lasts = process.create_map(train)
